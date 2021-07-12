a276a8dadcc18d7061fe311d2186d4ea9c2d2bc7
vamseevk <vamseevk2001@gmail.cmo>
2021-07-12 13:23:33 +0530

    initial commit

---
+++ app/src/main/java/vamsee/application/unscramble/MainActivity.kt
@@ -0,0 +1,11 @@
+package vamsee.application.unscramble
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
